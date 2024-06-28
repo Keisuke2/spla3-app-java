c90662f72d6100aad6c4067df53da934e84baa69
Keisuke2 <inokei1704@google.com>
2024-06-28 19:30:35 +0900

    アプリを起動するクラス

---
+++ src/main/java/schedule/AppLauncher.java
@@ -0,0 +1,9 @@
+package schedule;
+
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new SplaScheduleGui().setVisible(true));
+    }
+}
