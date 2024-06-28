7310765eadd94588aa2af1525cdf17403c82505d
Keisuke2 <inokei1704@google.com>
2024-06-28 19:19:33 +0900

    表示するテキストの大きさや色を個別に変えられるようにするクラス

---
+++ src/main/java/components/label/RuleLabel.java
@@ -0,0 +1,10 @@
+package components.label;
+
+import java.awt.*;
+
+public class RuleLabel extends TextLabel {
+    public RuleLabel(String text) {
+        super(text);
+        setFont(new Font("HiraMaruProN-W4", Font.PLAIN, 14));
+    }
+}
---
+++ src/main/java/components/label/StageNameLabel.java
@@ -0,0 +1,10 @@
+package components.label;
+
+import java.awt.*;
+
+public class StageNameLabel extends TextLabel {
+    public StageNameLabel(String text) {
+        super(text);
+        setFont(new Font("HiraMaruProN-W4", Font.PLAIN, 14));
+    }
+}
