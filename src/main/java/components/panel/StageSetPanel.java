ccc719d7f675f52244bc0f2c14de823987600b28
Keisuke2 <inokei1704@google.com>
2024-06-28 19:31:25 +0900

    ステージを表示するためのレイアウトを設定するクラス

---
+++ src/main/java/components/panel/StageSetPanel.java
@@ -0,0 +1,12 @@
+package components.panel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StageSetPanel extends JPanel {
+    public StageSetPanel() {
+        super();
+        setLayout(new FlowLayout());
+        setOpaque(false);
+    }
+}
