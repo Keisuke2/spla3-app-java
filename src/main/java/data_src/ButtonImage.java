675538add8ae16acd05b969f664e6caffa7c8481
Keisuke2 <inokei1704@google.com>
2024-06-28 19:17:33 +0900

    読み込むファイルのパスとURLを一元管理するクラス

---
+++ src/main/java/data_src/ButtonImage.java
@@ -0,0 +1,10 @@
+package data_src;
+
+public final class ButtonImage {
+    public static final String regular = "src/assets/regular.png";
+    public static final String bankara = "src/assets/bankara.png";
+    public static final String x = "src/assets/x.png";
+    public static final String event = "src/assets/event.png";
+    public static final String fest = "src/assets/fest.png";
+    public static final String salmonrun = "src/assets/salmonrun.png";
+}
